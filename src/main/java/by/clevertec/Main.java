d1992859c78c8d28daadc38c4fcc8999446357ac
Balay D <darvinbin@gmail.com>
2024-09-08 18:06:49 +0300

    Last changes

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -32,7 +32,7 @@ public class Main {
 //        task19();
 //        task20();
 //        task21();
-        task22();
+//        task22();
     }
 
     public static void task1() {
@@ -47,11 +47,16 @@ public class Main {
 
     public static void task2() {
         List<Animal> animals = Util.getAnimals();
-        animals.stream()
+        List<Animal> modifiedAnimals = animals.stream()
                 .filter(animal -> animal.getOrigin().equals("Japanese"))
-                .peek(animal -> animal.setBread(animal.getBread().toUpperCase()))
-                .forEach(System.out::println);
-    } // ??? Добавить наверно
+                .peek(animal -> {
+                    if ("Female".equals(animal.getGender())) {
+                        animal.setBread(animal.getBread().toUpperCase());
+                    }
+                })
+                .toList();
+        modifiedAnimals.forEach(System.out::println);
+    }
 
     public static void task3() {
         List<Animal> animals = Util.getAnimals();
@@ -73,108 +78,94 @@ public class Main {
 
     public static void task5() {
         List<Animal> animals = Util.getAnimals();
-        Optional<Animal> isHungurian = animals.stream()
-                .filter(animal -> animal.getAge() <= 30 && animal.getAge() >= 20)
-                .filter(animal -> animal.getOrigin().startsWith("Hungarian"))
-                .findAny();
-        if (isHungurian.isPresent()) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        boolean hasHungarian = animals.stream()
+                .filter(animal -> animal.getAge() >= 20 && animal.getAge() <= 30)
+                .anyMatch(animal -> animal.getOrigin().equals("Hungarian"));
+        System.out.println(hasHungarian ? "Yes" : "No");
     }
 
     public static void task6() {
         List<Animal> animals = Util.getAnimals();
-        Optional<Animal> isUnusualGender = animals.stream()
-                .filter(animal -> !animal.getGender().equals("Female") && !animal.getGender().equals("Male"))
-                .findAny();
-        if (isUnusualGender.isPresent()) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        boolean hasUnusualGender = animals.stream()
+                .anyMatch(animal -> !animal.getGender().equals("Female") && !animal.getGender().equals("Male"));
+        System.out.println(hasUnusualGender ? "Yes" : "No");
     }
 
     public static void task7() {
         List<Animal> animals = Util.getAnimals();
-        Optional<Animal> isOceaniaAnimal = animals.stream()
-                .filter(animal -> animal.getOrigin().equals("Oceania"))
-                .findAny();
-        if (isOceaniaAnimal.isPresent()) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        boolean hasNoOceaniaAnimals = animals.stream()
+                .noneMatch(animal -> animal.getOrigin().equals("Oceania"));
+        System.out.println(hasNoOceaniaAnimals ? "No" : "Yes");
     }
 
     public static void task8() {
         List<Animal> animals = Util.getAnimals();
         animals.stream()
-                .sorted(Comparator.comparing(Animal::getBread))
+                .sorted(Comparator.comparing(Animal::getBread)
+                        .thenComparingInt(Animal::getAge))
                 .limit(100)
-                .sorted(Comparator.comparingInt(Animal::getAge))
-                .skip(99)
+                .max(Comparator.comparingInt(Animal::getAge))
                 .map(Animal::getAge)
-                .forEach(System.out::println);
-    } // max
+                .ifPresent(System.out::println);
+
+    }
 
     public static void task9() {
         List<Animal> animals = Util.getAnimals();
-        char[] smallestArray = animals.stream()
+        int minLength = animals.stream()
                 .map(Animal::getBread)
                 .map(String::toCharArray)
-                .min(Comparator.comparingInt(arr -> arr.length))
-                .orElse(new char[0]);
-        System.out.println("smallestArray = " + smallestArray.length);
+                .mapToInt(arr -> arr.length)
+                .min()
+                .orElse(0);
+        System.out.println("Length of the smallest array = " + minLength);
     }
 
     public static void task10() {
         List<Animal> animals = Util.getAnimals();
-        Integer sum = animals.stream()
+        int sum = animals.stream()
                 .map(Animal::getAge)
-                .reduce(Integer::sum).orElse(0);
-        System.out.println("sum = " + sum);
+                .reduce(Integer::sum)
+                .orElse(0);
+        System.out.println("Sum = " + sum);
     }
 
     public static void task11() {
         List<Animal> animals = Util.getAnimals();
-        Long count = animals.stream()
+        long count = animals.stream()
                 .filter(animal -> animal.getOrigin().equals("Indonesian"))
                 .count();
-        Double sum = Double.valueOf(animals.stream()
+        int sum = animals.stream()
                 .filter(animal -> animal.getOrigin().equals("Indonesian"))
-                .map(Animal::getAge)
-                .reduce(Integer::sum).orElse(0));
-        System.out.println("average = " + sum / count);
+                .mapToInt(Animal::getAge)
+                .sum();
+        double average = (count > 0) ? (double) sum / count : 0.0;
+        System.out.println("Average = " + average);
     }
 
     public static void task12() {
         List<Person> persons = Util.getPersons();
         persons.stream()
                 .filter(person -> person.getGender().equals("Male"))
-                .filter(person -> person.getDateOfBirth().plusYears(18).isBefore(LocalDate.now()))
-                .filter(person -> person.getDateOfBirth().plusYears(27).isAfter(LocalDate.now()))
+                .filter(person -> {
+                    LocalDate now = LocalDate.now();
+                    int age = Period.between(person.getDateOfBirth(), now).getYears();
+                    return age >= 18 && age <= 27;
+                })
                 .sorted(Comparator.comparingInt(Person::getRecruitmentGroup))
                 .limit(200)
                 .forEach(System.out::println);
     }
 
-    private static int calculateAge(LocalDate birthDate) {
-        return Period.between(birthDate, LocalDate.now()).getYears();
-    }
-
     public static void task13() {
-
         final int MAX_EVACUATION_CAPACITY = 500;
         final int CHILD_AGE_THRESHOLD = 18;
         final int SENIOR_AGE_THRESHOLD = 65;
-
         List<House> houses = Util.getHouses();
         List<Person> peopleToEvacuate = houses.stream()
                 .flatMap(house -> house.getPersonList().stream())
                 .filter(person -> {
-                    int age = calculateAge(person.getDateOfBirth());
+                    int age = Period.between(person.getDateOfBirth(), LocalDate.now()).getYears();
                     return person.getOccupation().equalsIgnoreCase("Hospital")
                             || age < CHILD_AGE_THRESHOLD
                             || age >= SENIOR_AGE_THRESHOLD;
@@ -185,9 +176,8 @@ public class Main {
                     } else if (!p1.getOccupation().equalsIgnoreCase("Hospital") && p2.getOccupation().equalsIgnoreCase("Hospital")) {
                         return 1;
                     }
-
-                    int age1 = calculateAge(p1.getDateOfBirth());
-                    int age2 = calculateAge(p2.getDateOfBirth());
+                    int age1 = Period.between(p1.getDateOfBirth(), LocalDate.now()).getYears();
+                    int age2 = Period.between(p2.getDateOfBirth(), LocalDate.now()).getYears();
 
                     if (age1 < CHILD_AGE_THRESHOLD && age2 >= CHILD_AGE_THRESHOLD) {
                         return -1;
@@ -198,12 +188,10 @@ public class Main {
                     } else if (age1 < SENIOR_AGE_THRESHOLD && age2 >= SENIOR_AGE_THRESHOLD) {
                         return 1;
                     }
-
                     return 0;
                 })
                 .limit(MAX_EVACUATION_CAPACITY)
                 .toList();
-
         peopleToEvacuate.forEach(System.out::println);
     }
 
@@ -212,62 +200,57 @@ public class Main {
         final int MASS_THRESHOLD_ECHELON_2 = 1500;
         final int MASS_THRESHOLD_ECHELON_3 = 4000;
         final int PRICE_THRESHOLD_ECHELON_5 = 40000;
-
         List<Car> cars = Util.getCars();
         Map<String, List<Car>> echelonCars = new HashMap<>();
-        echelonCars.put("Turkmenistan", new ArrayList<>());
-        echelonCars.put("Uzbekistan", new ArrayList<>());
-        echelonCars.put("Kazakhstan", new ArrayList<>());
-        echelonCars.put("Kyrgyzstan", new ArrayList<>());
-        echelonCars.put("Russia", new ArrayList<>());
-        echelonCars.put("Mongolia", new ArrayList<>());
-
-        cars.stream()
-                .filter(car -> car.getCarMake().equalsIgnoreCase("Jaguar") || car.getColor().equalsIgnoreCase("White"))
-                .forEach(car -> echelonCars.get("Turkmenistan").add(car));
-
+        List.of("Turkmenistan", "Uzbekistan", "Kazakhstan", "Kyrgyzstan", "Russia", "Mongolia")
+                .forEach(country -> echelonCars.put(country, new ArrayList<>()));
         List<Car> remainingCars = cars.stream()
-                .filter(car -> !(car.getCarMake().equalsIgnoreCase("Jaguar") || car.getColor().equalsIgnoreCase("White")))
-                .collect(Collectors.toList());
-
-        remainingCars.stream()
-                .filter(car -> car.getMass() < MASS_THRESHOLD_ECHELON_2 &&
-                        List.of("BMW", "Lexus", "Chrysler", "Toyota").contains(car.getCarMake()))
-                .forEach(car -> echelonCars.get("Uzbekistan").add(car));
-
+                .filter(car -> {
+                    if (car.getCarMake().equalsIgnoreCase("Jaguar") || car.getColor().equalsIgnoreCase("White")) {
+                        echelonCars.get("Turkmenistan").add(car);
+                        return false;
+                    }
+                    return true;
+                })
+                .toList();
         remainingCars = remainingCars.stream()
-                .filter(car -> !(car.getMass() < MASS_THRESHOLD_ECHELON_2 &&
-                        List.of("BMW", "Lexus", "Chrysler", "Toyota").contains(car.getCarMake())))
-                .collect(Collectors.toList());
-
-        remainingCars.stream()
-                .filter(car -> (car.getColor().equalsIgnoreCase("Black") && car.getMass() > MASS_THRESHOLD_ECHELON_3) ||
-                        List.of("GMC", "Dodge").contains(car.getCarMake()))
-                .forEach(car -> echelonCars.get("Kazakhstan").add(car));
-
+                .filter(car -> {
+                    if (car.getMass() < MASS_THRESHOLD_ECHELON_2 &&
+                            List.of("BMW", "Lexus", "Chrysler", "Toyota").contains(car.getCarMake())) {
+                        echelonCars.get("Uzbekistan").add(car);
+                        return false;
+                    }
+                    return true;
+                })
+                .toList();
         remainingCars = remainingCars.stream()
-                .filter(car -> !((car.getColor().equalsIgnoreCase("Black") && car.getMass() > MASS_THRESHOLD_ECHELON_3) ||
-                        List.of("GMC", "Dodge").contains(car.getCarMake())))
-                .collect(Collectors.toList());
-
-        remainingCars.stream()
-                .filter(car -> car.getReleaseYear() < 1982 || List.of("Civic", "Cherokee").contains(car.getCarModel()))
-                .forEach(car -> echelonCars.get("Kyrgyzstan").add(car));
-
+                .filter(car -> {
+                    if ((car.getColor().equalsIgnoreCase("Black") && car.getMass() > MASS_THRESHOLD_ECHELON_3) ||
+                            List.of("GMC", "Dodge").contains(car.getCarMake())) {
+                        echelonCars.get("Kazakhstan").add(car);
+                        return false;
+                    }
+                    return true;
+                })
+                .toList();
         remainingCars = remainingCars.stream()
-                .filter(car -> !(car.getReleaseYear() < 1982 || List.of("Civic", "Cherokee").contains(car.getCarModel())))
-                .collect(Collectors.toList());
-
-        remainingCars.stream()
-                .filter(car -> (!List.of("Yellow", "Red", "Green", "Blue").contains(car.getColor())) || car.getPrice() > PRICE_THRESHOLD_ECHELON_5)
-                .forEach(car -> echelonCars.get("Russia").add(car));
-
+                .filter(car -> {
+                    if (car.getReleaseYear() < 1982 || List.of("Civic", "Cherokee").contains(car.getCarModel())) {
+                        echelonCars.get("Kyrgyzstan").add(car);
+                        return false;
+                    }
+                    return true;
+                })
+                .toList();
         remainingCars = remainingCars.stream()
-                .filter(car -> !(car.getColor().equalsIgnoreCase("Yellow") || car.getColor().equalsIgnoreCase("Red") ||
-                        car.getColor().equalsIgnoreCase("Green") || car.getColor().equalsIgnoreCase("Blue")) &&
-                        car.getPrice() <= PRICE_THRESHOLD_ECHELON_5)
-                .collect(Collectors.toList());
-
+                .filter(car -> {
+                    if (!List.of("Yellow", "Red", "Green", "Blue").contains(car.getColor()) || car.getPrice() > PRICE_THRESHOLD_ECHELON_5) {
+                        echelonCars.get("Russia").add(car);
+                        return false;
+                    }
+                    return true;
+                })
+                .toList();
         remainingCars.stream()
                 .filter(car -> car.getVin().contains("59"))
                 .forEach(car -> echelonCars.get("Mongolia").add(car));
@@ -281,40 +264,32 @@ public class Main {
             transportCosts.put(country, transportCost);
             totalRevenue += transportCost;
         }
+
         transportCosts.forEach((country, cost) -> System.out.println(country + ": " + String.format("%.2f", cost) + " $"));
         System.out.println("Total Revenue: " + String.format("%.2f", totalRevenue) + " $");
     }
 
-    private static double calculateTotalCost(Flower flower) {
-        final double WATER_COST_PER_CUBIC_METER = 1.39;
-        final int DAYS_IN_YEAR = 365;
-        final int YEARS = 5;
-        double waterConsumptionPerYear = flower.getWaterConsumptionPerDay() * DAYS_IN_YEAR;
-        double waterCostForFiveYears = (waterConsumptionPerYear * YEARS / 1000) * WATER_COST_PER_CUBIC_METER;
-        return flower.getPrice() + waterCostForFiveYears;
-    }
-
     public static void task15() {
-
         List<Flower> flowers = Util.getFlowers();
-        List<Flower> sortedFlowers = flowers.stream()
+        double totalCost = flowers.stream()
                 .sorted(Comparator
                         .comparing(Flower::getOrigin).reversed()
                         .thenComparing(Flower::getPrice)
                         .thenComparing(Comparator.comparing(Flower::getWaterConsumptionPerDay).reversed())
                 )
-                .toList();
-
-        List<Flower> selectedFlowers = sortedFlowers.stream()
                 .filter(flower -> flower.getCommonName().compareToIgnoreCase("C") >= 0 &&
                         flower.getCommonName().compareToIgnoreCase("S") <= 0)
                 .filter(flower -> flower.isShadePreferred() &&
                         flower.getFlowerVaseMaterial().stream().anyMatch(
                                 material -> List.of("Glass", "Aluminum", "Steel").contains(material)))
-                .toList();
-
-        double totalCost = selectedFlowers.stream()
-                .mapToDouble(Main::calculateTotalCost)
+                .mapToDouble(flower -> {
+                    final double WATER_COST_PER_CUBIC_METER = 1.39;
+                    final int DAYS_IN_YEAR = 365;
+                    final int YEARS = 5;
+                    double waterConsumptionPerYear = flower.getWaterConsumptionPerDay() * DAYS_IN_YEAR;
+                    double waterCostForFiveYears = (waterConsumptionPerYear * YEARS / 1000) * WATER_COST_PER_CUBIC_METER;
+                    return flower.getPrice() + waterCostForFiveYears;
+                })
                 .sum();
 
         System.out.println("Total maintenance cost: " + String.format("%.2f", totalCost) + " $");
@@ -326,7 +301,9 @@ public class Main {
                 .filter(student -> student.getAge() < 18)
                 .sorted(Comparator.comparing(Student::getSurname)
                         .thenComparingInt(Student::getAge))
-                .forEach(System.out::println);
+                .forEach(student -> System.out.println(
+                        "Surname: " + student.getSurname() + ", Age: " + student.getAge()));
+
     }
 
     public static void task17() {
@@ -368,22 +345,19 @@ public class Main {
                 .forEach(System.out::println);
     }
 
-    private static int findExam1ScoreByStudentId(int studentId, List<Examination> exams) {
-        return exams.stream()
-                .filter(exam -> exam.getStudentId() == studentId)
-                .findFirst()
-                .map(Examination::getExam1)
-                .orElse(0);
-    }
-
     public static void task20() {
         List<Student> students = Util.getStudents();
         List<Examination> examinations = Util.getExaminations();
+
         Map<String, List<Integer>> facultyExam1Scores = students.stream()
                 .collect(Collectors.groupingBy(
                         Student::getFaculty,
                         Collectors.mapping(
-                                student -> findExam1ScoreByStudentId(student.getId(), examinations),
+                                student -> examinations.stream()
+                                        .filter(exam -> exam.getStudentId() == student.getId())
+                                        .findFirst()
+                                        .map(Examination::getExam1)
+                                        .orElse(0),
                                 Collectors.toList()
                         )
                 ));
@@ -405,8 +379,8 @@ public class Main {
         List<Student> students = Util.getStudents();
         Map<String, Long> groupCount = students.stream()
                 .collect(Collectors.groupingBy(
-                        Student::getGroup,
-                        Collectors.counting()
+                                Student::getGroup,
+                                Collectors.counting()
                         )
                 );
         System.out.println(groupCount);
